eccf85738ba0b400fb6567d68fec1b83988836c8
SilicDev <83308290+SilicDev@users.noreply.github.com>
2026-09-06 14:04:20 +0200

    Add ixwebsocket library

--- framework/external/ixwebsocket/copy_ixwebsocket.bat
+++ framework/external/ixwebsocket/copy_ixwebsocket.bat
@@ -18,9 +18,13 @@ copy %InputDir%\ixwebsocket\ixwebsocket\*.h %OutputDir%\include\ixwebsocket
 echo.
 echo Copying LIBs...
 copy %InputDir%\ixwebsocket\buildWin32\Debug\ixwebsocket.lib %OutputDir%\lib\x86d\ixwebsocket
+copy %InputDir%\ixwebsocket\buildWin32\mbedtls_bin\library\Debug\*.lib %OutputDir%\lib\x86d\ixwebsocket
 copy %InputDir%\ixwebsocket\buildWin32\Release\ixwebsocket.lib %OutputDir%\lib\x86\ixwebsocket
+copy %InputDir%\ixwebsocket\buildWin32\mbedtls_bin\library\Release\*.lib %OutputDir%\lib\x86\ixwebsocket
 copy %InputDir%\ixwebsocket\build\Debug\ixwebsocket.lib %OutputDir%\lib\x64d\ixwebsocket
+copy %InputDir%\ixwebsocket\build\mbedtls_bin\library\Debug\*.lib %OutputDir%\lib\x64d\ixwebsocket
 copy %InputDir%\ixwebsocket\build\Release\ixwebsocket.lib %OutputDir%\lib\x64\ixwebsocket
+copy %InputDir%\ixwebsocket\build\mbedtls_bin\library\Release\*.lib %OutputDir%\lib\x64\ixwebsocket
 
 :: Done
 echo.
